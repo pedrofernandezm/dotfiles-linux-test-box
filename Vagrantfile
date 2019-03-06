1a18d52639cc0427e8f8eb09f253ea085561f69d
Pedro Fernandez <pedrofernandezm@gmail.com>
2019-03-06 16:43:54 -0500

    Allow SSH forward agent during provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,5 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
   config.vm.provision "shell", path: "shell-provision.sh"
+  config.ssh.forward_agent = true
 end
--- shell-provision.sh
+++ shell-provision.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo apt-get -y install git
+set -e
 
+sudo apt-get update
+sudo apt-get -y install git
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 git clone git@github.com:pedrofernandezm/dotfiles.git
+
+echo "PROVISIONING DONE!!!"
