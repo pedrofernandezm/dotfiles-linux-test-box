1403c22a504f7d85729848ee92f0be3e3414fa94
Pedro Fernandez <pedrofernandezm@gmail.com>
2019-03-06 16:52:58 -0500

    Installing VIM

--- shell-provision.sh
+++ shell-provision.sh
@@ -3,7 +3,7 @@
 set -e
 
 sudo apt-get update
-sudo apt-get -y install git
+sudo apt-get -y install git vim
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 git clone git@github.com:pedrofernandezm/dotfiles.git
 
